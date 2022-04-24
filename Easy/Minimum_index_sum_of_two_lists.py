588b59107b32c8d22c9b8007ba81b784df3ca64a
varunkuntal <varunkuntal@yahoo.in>
2022-04-24 09:12:56 +0530

    Day 34 of #100DaysOfCode - Unique email addresses - optimized TC:O(n) SC:O(n)

--- Easy/Minimum_index_sum_of_two_lists.py
+++ Easy/Minimum_index_sum_of_two_lists.py
@@ -1,14 +1,18 @@
+#Problem URL: https://leetcode.com/problems/minimum-index-sum-of-two-lists/submissions/
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        l1 = {}
         common = {}
         n1 = len(list1)
         n2 = len(list2)
-        
+                
         for i in range(n1):
-            for j in range(n2):
-                if list1[i] == list2[j]:
-                    common[list1[i]] = i+j
-                    
+            l1[list1[i]] = i+1
+            
+        for j in range(n2):
+            if l1.get(list2[j]):
+                common[list2[j]] = l1[list2[j]] + j
+
         minix = min(common.values())
         
         return [i for i, j in common.items() if j == minix]
